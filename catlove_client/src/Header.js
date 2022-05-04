2a71c44331281ffeb4b18ea66f948de02aa7d0e6
knu9910 <knu9910@naver.com>
2022-05-05 00:51:49 +0900

    initial setting, create header

---
+++ catlove_client/src/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faBasketShopping } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faBell } from '@fortawesome/free-regular-svg-icons';
+
+function Header() {
+  return (
+    <header>
+      <div className="bg-amber-200 font-Patrick text-center text-xl">
+        Cat Shoppingmol
+      </div>
+      <div className="flex justify-center m-20 mt-20">
+        <div className="font-Patrick text-4xl mr-1.5">Cat Love</div>
+        <div className="text-2xl flex justify-around w-[30%] basis-6/12 font-Patrick mt-1.5">
+          <div>Shop</div>
+          <div>Best</div>
+          <div>Board</div>
+          <div>C/S</div>
+        </div>
+        <div className="flex text-3xl w-[20%] mt-1.5 justify-end">
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faUser} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faBasketShopping} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faBell} />
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center bg-transparent h-96">
+        <img src="img/cat1.jpg" />
+        <img src="img/catsflower.png" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ catlove_client/src/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function hmepage() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default hmepage;
